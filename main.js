379dd5e74eeb585191504f5261053348e21f27c7
Alice Goland <alisagl90@gmail.com>
2026-09-16 17:22:47 +0300

    Ball transition smoothly and resetreset size of ball after400px after 400px

--- main.js
+++ main.js
@@ -2,8 +2,18 @@
 
 function onBallClick(elBall) {
     elBall.offsetWidth
+    console.log(elBall.offsetWidth)
+
 
     elBall.style.width = elBall.offsetWidth + 50 + 'px'
     elBall.style.height = elBall.offsetWidth + 50 + 'px'
     elBall.innerHTML = elBall.style.width
+
+    if (elBall.offsetWidth >400) {
+        elBall.style.width = 100 + 'px'
+        elBall.style.height = 100 + 'px'
+        elBall.innerHTML = elBall.style.width
+
+    }
+    console.log(elBall.offsetWidth)
 }
