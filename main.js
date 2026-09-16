61cfe0f3b3f847c11370d932478eb51f9551479d
Alice Goland <alisagl90@gmail.com>
2026-09-16 16:40:29 +0300

    Ball gets bigger on click

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 'use strict'
 
-function onBallClick(elBall){
-    console.log('I am a ball')
+function onBallClick(elBall) {
+    elBall.offsetWidth
+
+    elBall.style.width = elBall.offsetWidth + 50 + 'px'
+    elBall.style.height = elBall.offsetWidth + 50 + 'px'
+    elBall.innerHTML = elBall.style.width
 }
