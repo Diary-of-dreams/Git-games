f78ed649eb95b03744b1e9cc746a47c6e133ed00
Alice Goland <alisagl90@gmail.com>
2026-09-17 12:35:54 +0300

    Add third ball to change color and size of ball1 and 2

--- main.js
+++ main.js
@@ -8,13 +8,23 @@ function onBallClick(elBall, maxDiameter) {
 
     const randomSize = elBall.offsetWidth + getRandomInt(20, 60) +'px'
     elBall.style.width = randomSize
-    elBall.style.height = randomSize 
     elBall.innerHTML = randomSize
     if (elBall.offsetWidth > maxDiameter) {
         elBall.style.width = 100 + 'px'
-        elBall.style.height = 100 + 'px'
         elBall.innerHTML = elBall.style.width
 
     }
     console.log(elBall.offsetWidth)
+}
+
+function onBallClickThird(elClick) {
+const firstBall = document.querySelector('.ball')
+const secondBall = document.querySelector('.ball2')
+
+firstBall.style.backgroundColor = 'yellow'
+firstBall.style.width = '250px'
+firstBall.innerHTML = '250'
+secondBall.style.backgroundColor = 'red'
+secondBall.style.width = '144px'
+secondBall.innerHTML = '144'
 }
